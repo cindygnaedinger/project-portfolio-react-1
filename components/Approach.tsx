b4f676b5d3817d94b3fbb99e4bc55c089c24254d
cindygnaedinger <100149373+cindygnaedinger@users.noreply.github.com>
2025-02-25 21:09:53 -0300

    Versioning untracked files

--- components/Approach.tsx
+++ components/Approach.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React from "react";
-
+// ?
 import { AnimatePresence, motion } from "framer-motion";
 import { CanvasRevealEffect } from "@/components/ui/CanvasRevealEffect";
 
